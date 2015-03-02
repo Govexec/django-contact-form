3a39843ea075cf5279db5ec5a5d67a261b2ba39a
Christopher Greinke <cgreinke@govexec.com>
2015-03-02 14:38:27 -0500

    urls.defaults is no longer correct for urls changing to use class based template view

--- contact_form/urls.py
+++ contact_form/urls.py
@@ -9,20 +9,13 @@ hierarchy (for best results with the defaults, include it under
 ``/contact/``).
 
 """
-
-
-from django.conf.urls.defaults import *
-from django.views.generic.simple import direct_to_template
+from django.conf.urls import *
+from django.views.generic.base import TemplateView
 
 from contact_form.views import contact_form
 
 
 urlpatterns = patterns('',
-                       url(r'^$',
-                           contact_form,
-                           name='contact_form'),
-                       url(r'^sent/$',
-                           direct_to_template,
-                           { 'template': 'contact_form/contact_form_sent.html' },
-                           name='contact_form_sent'),
-                       )
+    url(r'^$', contact_form, name='contact_form'),
+    url(r'^sent/$', TemplateView.as_view(template_name='contact_form/contact_form_sent.html'), name='contact_form_sent'),
+)
